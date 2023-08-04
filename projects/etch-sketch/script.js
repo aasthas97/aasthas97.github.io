c95e65d60351f9c9481790f9deb5f5e6e3e3fe77
Aastha <aasthashar@gmail.com>
2023-08-04 18:45:06 +0200

    First finished implementation

--- projects/etch-sketch/script.js
+++ projects/etch-sketch/script.js
@@ -1,20 +1,60 @@
+const gridColor = "rgb(245, 245, 245)";
+let drawColor;
+
 const container = document.querySelector("#grid-container");
-var chosenColor = "rgb(177, 34, 146)";
+const colorPicker = document.getElementById("color-picker");
+const sizePicker = document.getElementById("size-picker");
 
-function changeColor() {
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+}
+
+function ChangeSize() {
+  gridSize = sizePicker.value;
+  Reset(gridSize);
+}
+
+function ChangeColour() {
+  var foo = hexToRgb(colorPicker.value);
+  drawColor = `rgb(${foo.r}, ${foo.g}, ${foo.b})`;
+}
+
+function sketch() {
   this.style.backgroundColor =
-    this.style.backgroundColor == chosenColor ? "whitesmoke" : chosenColor;
+    this.style.backgroundColor == drawColor ? gridColor : drawColor;
 }
 
-for (let boxNumber = 0; boxNumber < 256; boxNumber++) {
-  let sketchBox = document.createElement("div");
-  sketchBox.setAttribute("class", "sketch-box");
-  sketchBox.addEventListener("click", changeColor);
-  container.appendChild(sketchBox);
+function DrawGrid(gridSize) {
+  boxSize = 800 / gridSize;
+  for (let boxNumber = 0; boxNumber < gridSize ** 2; boxNumber++) {
+    let sketchBox = document.createElement("div");
+    sketchBox.style.width = boxSize + "px";
+    sketchBox.style.height = boxSize + "px";
+    sketchBox.setAttribute("class", "sketch-box");
+    sketchBox.addEventListener("click", sketch);
+    container.appendChild(sketchBox);
+  }
 }
+
+function Reset(gridSize) {
+  container.innerHTML = "";
+  container.style.backgroundColor = gridColor;
+  colorPicker.value = "#b12292";
+  drawColor = "rgb(177, 34, 146)";
+  DrawGrid(gridSize);
+}
+
+Reset(16);
 
 /// TODO
-// 1. Add grid size selector
-// 2. Add color selector
-// 3. Clear all/reset option
 // 4. Define colors as CSS variables
+// 5. Make jS concise
+// 6. Draw by click and drag
+// 7. Make phone-friendly
